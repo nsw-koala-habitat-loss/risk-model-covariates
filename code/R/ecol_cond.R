ae2e025c2f5d80d50ac7a9e6f6432cd5118e5cae
Yi Fei Chung <uqychun7@uq.edu.au>
2024-06-11 20:00:54 +1000

    Reprocess due to missing output

--- code/R/ecol_cond.R
+++ code/R/ecol_cond.R
@@ -11,3 +11,14 @@ ecol_cond %>%
   clipRast(name = "ecol_cond", apply_mask = FALSE, to_output = FALSE, overwrite = TRUE) %>%
   resampleRast(name = "ecol_cond", overwrite = TRUE) %>%
   clipRast(name = "ecol_cond", to_output = TRUE, overwrite = TRUE)
+
+### Reprocess missing data###
+Woody <- rast("Input/woody_nsw.tif")
+
+ecol_cond <- rast("Input/biodiversityecologicalconditionofterrestrialhabitat/bba_31a_cond_V2_nsw_raw.tif") %>% 
+  project(crs(Woody), thread = TRUE) %>% 
+  resample(Woody, method = "bilinear", thread = TRUE) %>% 
+  crop(Woody, snap = "out" , mask = TRUE)
+names(ecol_cond) <- "EcolCond"
+
+writeRaster(ecol_cond, "Output/Raster/EcolCond.tif", overwrite = TRUE)
