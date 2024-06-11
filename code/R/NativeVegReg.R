e1f2399ebe9debd7ad5f7fc5607042b14806450d
Yi Fei Chung <uqychun7@uq.edu.au>
2024-06-11 20:01:59 +1000

    Chnaging input to 12122022 files

--- code/R/NativeVegReg.R
+++ code/R/NativeVegReg.R
@@ -38,19 +38,34 @@ plot(NSWbound)
 ### 3 category 2-vulnerable regulated land 
 ### 4 category 2-sensitive regulated land 
 ### 6 category 2-sensitive and vulnerable regulated lands areas of overlap
-naluma_nsw_2017_abkl0 <- rast("Input/native_vegetation_regulatory_map_datapack_version11_20240522/naluma_nsw_2017_abkl0_c20240514_u11.tif")
 
+# Version: 20240514----
+naluma_nsw_2017_abkl0 <- rast("Input/native_vegetation_regulatory_map_datapack_version11_20240522/naluma_nsw_2017_abkl0_c20240514_u11.tif")
 ## naluma_nsw_2017_abel0 layer contains 1 map display class
+
 ### 5 Land excluded from the LLS Act
 naluma_nsw_2017_abel0 <- rast("Input/native_vegetation_regulatory_map_datapack_version11_20240522/naluma_nsw_2017_abel0_c20240514_u11.tif")
 
-naluma_nsw_2017_rast <- rast(list(naluma_nsw_2017_abel0, naluma_nsw_2017_abkl0))
-names(naluma_nsw_2017_rast) <- c("Excluded" , "Cat2")
+# Version: 20221212----
+## naluma_nsw_2017_abel0 layer contains 1 map display class
+naluma_nsw_2017_abkl0 <- rast("Input/native_vegetation_regulatory_map_datapack_version4_20221212/naluma_nsw_2017_abkl0_c20221212_u9.tif") 
+### 5 Land excluded from the LLS Act
+naluma_nsw_2017_abel0 <- rast("Input/native_vegetation_regulatory_map_datapack_version4_20221212/naluma_nsw_2017_abel0_c20221212_u9.tif")
 
-NatVegReg_rast <- ifel(not.na(naluma_nsw_2017_rast$Excluded), naluma_nsw_2017_rast$Excluded, naluma_nsw_2017_rast$Cat2)
+Woody_NVR <- project(Woody, crs(naluma_nsw_2017_abkl0), thread = TRUE)
 
-NatVegReg_rast <- resample(NatVegReg_rast, Woody, method = "mode", threads = TRUE) %>% 
-  crop(Woody,snap="out", mask = TRUE, filename = "Output/NatVegReg.tif", overwrite = TRUE)
+NVR_Cat2_pNVR <- resample(naluma_nsw_2017_abkl0, Woody_NVR, method = "mode", threads = TRUE)
+NVR_Excluded_pNVR <- resample(naluma_nsw_2017_abel0, Woody_NVR, method = "mode", threads = TRUE)
 
-NatVegReg_rast <- sum(NatVegReg_rast$Cat2, Woody_template$EXT, na.rm = TRUE)
-writeRaster(NatVegReg_rast, "Output/NatVegReg.tif", overwrite = TRUE)
+NVR_2lyr_pNVR <- rast(list(NVR_Cat2_pNVR, NVR_Excluded_pNVR))
+names(NVR_pNVR) <- c("Cat2", "Excluded")
+
+NVR_pNVR <- ifel(not.na(NVR_2lyr_pNVR$Excluded), NVR_2lyr_pNVR$Excluded, NVR_2lyr_pNVR$Cat2)
+
+NVR <- project(NVR_pNVR, crs(Woody), thread = TRUE)
+
+NatVegReg <- ifel(not.na(NVR$Cat2), NVR$Cat2, Woody_template$EXT)
+names(NatVegReg) <- "NatVegReg"
+writeRaster(NatVegReg, "Output/Raster/NatVegReg.tif", overwrite = TRUE)
+NatVegReg <- rast("Output/Raster/NatVegReg.tif")
+plot(NatVegReg)
