041c37b717c48f795f11f6f70d1d6f1e6ce7dfd1
Yi Fei Chung <uqychun7@uq.edu.au>
2024-06-15 13:03:57 +1000

    reclassified

--- code/R/LandTenure.R
+++ code/R/LandTenure.R
@@ -16,39 +16,43 @@ library(ggpubr)
 
 # load raster template
 
-Woody <- rast("Input/woody_nsw.tif")
-Woody_template <- rast("Input/Woody_template.tif")
+Woody <- rast("D:/Data/NSW_Deforestation/risk-model-covariates/Input/woody_nsw.tif")
+Woody_template <- rast("D:/Data/NSW_Deforestation/risk-model-covariates/Input/Woody_template.tif")
 
-NSW_vect <- vect("Input/STE_2021_AUST_SHP_GDA2020/STE_2021_AUST_GDA2020.shp") %>% 
+NSW_vect <- vect("D:/Data/NSW_Deforestation/risk-model-covariates/Input/STE_2021_AUST_SHP_GDA2020/STE_2021_AUST_GDA2020.shp") %>% 
   tidyterra::filter(STE_NAME21 == "New South Wales")
 
 #Load land tenure 
 
-AusLandTen <- rast("Input/land_tenure_of_australia_2010_11_to_2015_16_20210929/AUSTEN_250m_2015_16_alb/AUSTEN_250m_2015_16/AUSTEN_250m_2015_16_alb.tif")
-NPWS_Land <- vect("Input/npws_allmanagedland/NPWS_AllManagedLand.shp")
+AusLandTen <- rast("D:/Data/NSW_Deforestation/risk-model-covariates/Input/land_tenure_of_australia_2010_11_to_2015_16_20210929/AUSTEN_250m_2015_16_alb/AUSTEN_250m_2015_16/AUSTEN_250m_2015_16_alb.tif")
+# NPWS_Land <- vect("D:/Data/NSW_Deforestation/risk-model-covariates/Input/npws_allmanagedland/NPWS_AllManagedLand.shp")
 
-NSW_vect_Albers <- project(NSW_vect, crs(AusLandTen))
+AusLandTen <- project(AusLandTen, crs(Woody), method = "mode", thread = TRUE)
 
 NPWS_Land_df <- as.data.frame(NPWS_Land)
 NPWS_Land_df %>% distinct(IUCN , TENURETYPE)
 plot(NPWS_Land)
 
-NSWLandTen <- crop(AusLandTen, NSW_vect_Albers, snap = "out", mask = TRUE) %>% 
-  project(crs(Woody))
+NSWLandTen <- AusLandTen %>% 
+  catalyze() %>% 
+  subset("L2N") %>% 
+  project(crs(Woody), method = "mode", thread = TRUE)%>% 
+  resample(y = Woody, method = "mode", thread = TRUE) %>% 
+  crop(Woody, snap = "out", mask = TRUE)
+
+names(NSWLandTen) <- "LandTen"
+
+NSWLandTen <- ifel(not.na(NSWLandTen$LandTen), NSWLandTen$LandTen, Woody_template$EXT)
+NSWLandTen <- NSWLandTen %>% classify(cbind(c(0, 10, 21, 22,23), c(0, 1, 2, 3, 4)))
+names(NSWLandTen) <- "LandTen"
+plot(NSWLandTen)
+writeRaster(NSWLandTen, "Output/Raster/LandTen.tif", overwrite = TRUE)
+
+NSWLandTen <- rast("Output/Raster/LandTen.tif")
 
 NSWLandTen_lut <- as.data.frame(cats(NSWLandTen))[,c(4:5)] %>% 
   distinct()
 write.csv(NSWLandTen_lut, "Output/NSWLandTen_lut.csv")
 
-NSWLandTen_L2 <- catalyze(NSWLandTen) %>% 
-  subset("L2N") %>% 
-  resample(y = Woody, thread = TRUE) %>% 
-  crop(Woody, snap = "out", mask = TRUE)
+plot(NSWLandTen)
 
-names(NSWLandTen_L2) <- "LandTen"
-
-writeRaster(NSWLandTen_L2, "Output/Raster/LandTen.tif", overwrite = TRUE)
-
-LandTen <- rast("Output/Raster/LandTen.tif")
-
-plot(LandTen)
